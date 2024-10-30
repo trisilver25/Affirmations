f660f001c17f7c17e366ec917186dfeeba044f11
Tristin Smith <85770698+trisilver25@users.noreply.github.com>
2024-10-30 18:56:29 -0400

    Initial Commit w/ new composable function and preview.

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -19,11 +19,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.example.affirmations.model.Affirmation
 import com.example.affirmations.ui.theme.AffirmationsTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.material3.Text
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.affirmations.data.Datasource
+
 
 class MainActivity : ComponentActivity() {
 
@@ -45,4 +58,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun AffirmationsApp() {
+}
+
+@Composable
+fun AffirmationCard(affirmation: Affirmation, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+      Column {
+        Image(
+            painter = painterResource(affirmation.imageResourceId),
+            contentDescription =  stringResource(affirmation.stringResourceId),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(194.dp),
+            contentScale = ContentScale.Crop
+        )
+          Text(
+              text = LocalContext.current.getString(affirmation.stringResourceId),
+              modifier = Modifier.padding(16.dp),
+              style = MaterialTheme.typography.headlineSmall
+          )
+      }
+    }
+}
+
+@Preview
+@Composable
+private fun AffirmationCardPreview() {
+    Affirmation(Affirmation(R.string.affirmation1, R.drawable.image1))
 }
